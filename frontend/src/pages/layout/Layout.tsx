74ef6e78f7db36c727c56aa7d67ff2795377ca4e
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2024-01-29 11:06:01 +0100

    White logo

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-import Cat from "../../assets/cat.svg";
+import CatWhite from "../../assets/cat-white.svg";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
 import { Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles } from "@fluentui/react";
 import { useContext, useEffect, useState } from "react";
@@ -47,7 +47,7 @@ const Layout = () => {
                 <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
                     <Stack horizontal verticalAlign="center">
                         <img
-                            src={Cat}
+                            src={CatWhite}
                             className={styles.headerIcon}
                             aria-hidden="true"
                         />
